3d3d4531d6f25fb318e0883b7f7c63421c50f396
Ines <inesrahrah@gmail.com>
2024-07-28 17:15:29 +0200

    Add files via upload
    "final touches, update HTML and CSS"

--- data_fetcher.py
+++ data_fetcher.py
@@ -2,30 +2,28 @@ import requests
 import os
 from dotenv import load_dotenv
 
-
 def fetch_data(animal_name):
-  """Fetches the animals data for the animal 'animal_name' from an API.
+    """Fetches the animal's data for the animal 'animal_name' from an API.
 
-  Args:
-      animal_name (str): The name of the animal to search for.
+    Args:
+        animal_name (str): The name of the animal to search for.
 
-  Returns:
-      list: A list of animal dictionaries containing information (may be empty if not found).
-  """
-  load_dotenv()
+    Returns:
+        list: A list of animal dictionaries containing information (may be empty if not found).
+    """
+    load_dotenv()
 
-  API_KEY = os.getenv('API_KEY') 
-  
-  api_url = "https://api.api-ninjas.com/v1/animals"
-  headers = {'X-Api-Key': API_KEY}
-  params = {'name': animal_name}
+    API_KEY = os.getenv('API_KEY')
 
-  response = requests.get(api_url, headers=headers, params=params)
+    api_url = "https://api.api-ninjas.com/v1/animals"
+    headers = {'X-Api-Key': API_KEY}
+    params = {'name': animal_name}
 
-  if response.status_code == requests.codes.ok:
-    # Parse the JSON response
-    data = response.json()
-    return data
-  else:
-    print(f"API request failed with status code: {response.status_code}")
-    return []  # Return an empty list on error
+    response = requests.get(api_url, headers=headers, params=params)
+
+    if response.status_code == requests.codes.ok:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        return []  # Return an empty list on error
